308489cf4c20b086ee622ee22a0110dfaadc8d68
ittok49-code <itto.k.49@gmail.com>
2026-08-10 14:41:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,50 @@
-const CACHE_NAME = 'future-gauge-v2';
+const CACHE_NAME = 'future-gauge-v3';
 
 const ASSETS = [
-  './index.html',
-  './manifest.webmanifest',
-  './431~2.jpg'
+'./index.html',
+'./manifest.webmanifest',
+'./431~2.jpg'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+return cache.addAll(ASSETS);
+})
+);
 
-  self.skipWaiting();
+self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
+event.waitUntil(
+caches.keys().then((keys) => {
+return Promise.all(
+keys.map((key) => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+);
+}).then(() => self.clients.claim())
+);
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+event.respondWith(
+fetch(event.request)
+.then((response) => {
+const copy = response.clone();
+
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.put(event.request, copy);
+    });
+
+    return response;
+  })
+  .catch(() => {
+    return caches.match(event.request);
+  })
+
+);
 });
